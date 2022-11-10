612ce309c8c890f7bfd0d013314471daf4e2eaee
Pablo Di Pietro <pablod.dipietro@gmail.com>
2022-11-10 13:16:13 -0300

    svg corregidos

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -21,14 +21,27 @@ export default function Navbar() {
           <li>PAGES</li>
           <li>SHORTCODES</li>
           <li>POST TYPES</li>
-          <li>
-            <Image src={fav} height="20" alt="icono favorito" />
+          <li className={s.iconos}>
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-heart" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#486683" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M19.5 13.572l-7.5 7.428l-7.5 -7.428m0 0a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
+            </svg>
           </li>
           <li>
-            <Image src={cart} alt="icono carrito" height="20" />
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#486683" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <circle cx="6" cy="19" r="2" />
+              <circle cx="17" cy="19" r="2" />
+              <path d="M17 17h-11v-14h-2" />
+              <path d="M6 5l14 1l-1 7h-13" />
+            </svg>
           </li>
           <li>
-            <Image src={search} alt="icono search" height="20" />
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#486683" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <circle cx="10" cy="10" r="7" />
+              <line x1="21" y1="21" x2="15" y2="15" />
+            </svg>
           </li>
         </ul>
       </nav>
--- components/SearchBar/index.js
+++ components/SearchBar/index.js
@@ -12,7 +12,14 @@ export default function index() {
         placeholder="Search products..."
       />
       <div className={s.contenedor_img}>
-        <Image src={buscar} height="25" className={s.imagen} alt="Lupa" />
+        <div className={s.imagen}>
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="15" height="15" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <circle cx="10" cy="10" r="7" />
+              <line x1="21" y1="21" x2="15" y2="15" />
+            </svg>
+        </div>
+        {/* <Image src={buscar} height="25" className={s.imagen} alt="Lupa" /> */}
       </div>
     </div>
   );
