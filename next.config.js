7b50c577c6a6ba0baa76fb1f40bd948e3df133a6
Pablo Di Pietro <89948408+Srjlee@users.noreply.github.com>
2023-01-20 08:45:17 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,16 +2,7 @@
 
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    CLOUD_NAME: "dbzk3zeb6",
-    API_KEY: "746543423797438",
-    API_SECRET: "MVWgWZ-ah-m18_5kBdRRssImAcM",
-    CLOUDINARY_URL:
-      "cloudinary://746543423797438:MVWgWZ-ah-m18_5kBdRRssImAcM@dbzk3zeb6",
-  },
-  images: {
-    domains: ["asset.cloudinary.com"],
-  },
+
 };
 
 module.exports = nextConfig;
